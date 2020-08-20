c37f8d8d23bd7454d833e5aa36cb1de8894a511e
MatejGomboc <matejg03@gmail.com>
2020-08-20 19:53:09 +0200

    Login working ok.

--- public/login_session_create.php
+++ public/login_session_create.php
@@ -9,9 +9,9 @@ if (!empty($_SESSION["userid"])) {
 
 $db = new SQLite3("../private/credentials.db", SQLITE3_OPEN_READONLY);
 
-$statement = $db->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
-$statement->bindValue(":username", $_POST["username"]);
-$statement->bindValue(":password", $_POST["password"]);
+$statement = $db->prepare("SELECT id FROM users WHERE username = :username AND password = :password");
+$statement->bindValue(":username", $_POST["username"], SQLITE3_TEXT);
+$statement->bindValue(":password", $_POST["password"], SQLITE3_TEXT);
 $results = $statement->execute();
 
 if ($results == NULL) {
@@ -19,17 +19,24 @@ if ($results == NULL) {
     return;
 }
 
-if (!$results->numColumns()) {
+$numColumns = $results->numColumns();
+
+if ($results->numColumns() == 0) {
     header("Location: login_form.php");
     return;
 }
 
-if ($results->columnType(0) == SQLITE3_NULL) {
+$entry = $results->fetchArray(SQLITE3_ASSOC);
+
+$columnName = $results->columnName(0);
+$columnType = $results->columnType(0);
+
+if ($results->columnType(0) != SQLITE3_INTEGER) {
     header("Location: login_form.php");
     return;
 }
 
-$_SESSION["userid"] = $results->fetchArray(SQLITE3_ASSOC)["id"];
+$_SESSION["userid"] = $entry["id"];
 
 $db->close();
 
